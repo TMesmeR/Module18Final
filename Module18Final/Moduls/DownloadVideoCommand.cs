7af8b00a292f26e60659634b76ef2d78f8754ff6
TMesmeR <mesmerat@gmail.com>
2024-06-28 10:45:47 +0300

    Добавьте файлы проекта.

---
+++ Module18Final/Moduls/DownloadVideoCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
+
+namespace Module18Final.Moduls
+{
+    internal class DownloadVideoCommand : ICommand
+    {
+        private readonly YoutubeClient _youtubeClient;
+        private readonly string _videoUrl;
+        private readonly string _outputFilePath;
+
+        public DownloadVideoCommand(YoutubeClient youtubeClient, string videoUrl, string outputFilePath)
+        {
+            _youtubeClient = youtubeClient;
+            _videoUrl = videoUrl;
+            _outputFilePath = outputFilePath;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            await _youtubeClient.Videos.DownloadAsync(_videoUrl, _outputFilePath, builder => builder.SetPreset(ConversionPreset.Fast));
+            Console.WriteLine($"Видео сохранено по пути : {Path.GetFullPath (_outputFilePath)}.");
+        }
+    }
+}
---
+++ Module18Final/Moduls/GetVideoInfoCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+
+namespace Module18Final.Moduls
+{
+    public class GetVideoInfoCommand : ICommand
+    {
+        private readonly YoutubeClient _youtubeClient;
+        private readonly string _videoUrl;
+
+        public GetVideoInfoCommand(YoutubeClient youtubeClient, string videoUrl)
+        {
+            _youtubeClient = youtubeClient;
+            _videoUrl = videoUrl;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            var video = await _youtubeClient.Videos.GetAsync(_videoUrl);
+            Console.WriteLine($"Название: {video.Title}");
+            Console.WriteLine($"Описание: {video.Description}");
+        }
+    }
+}
